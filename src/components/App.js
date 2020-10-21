ed71760f68ac248b07c4581e608ffbd1756632bf
Alok8777 <68750984+Alok8777@users.noreply.github.com>
2020-10-21 20:30:55 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -7,9 +7,15 @@ class App extends Component {
 	};
 
     render() {
+		const paragraph = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
+				<button id="click">
+					<p id="para">{paragraph}</p>
+				</button>
+
     		</div>
     	);
     }
