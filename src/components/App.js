f866fad74d8c0cd662ec2b8e62d549cfd6078096
Alok8777 <68750984+Alok8777@users.noreply.github.com>
2020-11-28 20:55:47 +0530

    Change

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,37 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
-
-    render() {
-		const paragraph = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
-
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-				<button id="click">
-					<p id="para">{paragraph}</p>
-				</button>
-
-    		</div>
-    	);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      paragraph: false
+    };
+  }
+  renderPara() {
+    return (
+      <p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
+    );
+  }
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove this main div!! */}
+        <button
+          id="click"
+          onClick={() => {
+            this.setState({ paragraph: true });
+          }}
+        >
+          Click
+        </button>
+        {this.state.paragraph ? this.renderPara() : ""}
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
